9f849d9a612d994b0dcfb5dcdf5da2716888404c
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-19 13:23:11 +0100

    added room name in setUp to allow name test to pass - had to edit some parts of test as it had not been initially written correctly

--- tests/room_test.py
+++ tests/room_test.py
@@ -4,5 +4,8 @@ from src.room import Room
 
 class TestRoom(unittest.TestCase):
 
-     def test_room_has_name(self):
-        self.assertEqual(Bowie, self.room.name)
+    def setUp(self):
+        self.room_1 = Room("Bowie")
+
+    def test_room_has_name(self):
+        self.assertEqual("Bowie", self.room_1.name)
