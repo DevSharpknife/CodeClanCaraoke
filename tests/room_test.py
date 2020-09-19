ce8744e697f7449e61ac702fe5414d7ab1e1ca4c
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-19 13:50:17 +0100

    Added test for adding song to songlist

--- tests/room_test.py
+++ tests/room_test.py
@@ -9,4 +9,6 @@ class TestRoom(unittest.TestCase):
 
     def test_room_has_name(self):
         self.assertEqual("Bowie", self.room_1.name)
-        
+
+    def test_add_song_to_song_list(self):
+        self.assertEqual(1, len(self.room.song_list))
