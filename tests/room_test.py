fb8608069c225d1a7da9fc3c1a9a83f2c26011d4
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-19 16:55:46 +0100

    Added more songs and now the add songs test does not work as expected

--- tests/room_test.py
+++ tests/room_test.py
@@ -6,19 +6,22 @@ from src.song import Song
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
+        self.room = Room("Bowie")
         self.song_1 = Song("Juice", "Lizzo")
         self.song_2 = Song("Dr Dre", "Forgot About Dre")
-        self.song_list = [self.song_1]
-        self.room_1 = Room("Bowie")
+        self.song_3 = Song("David Byrne", "Finite=Alright")
+        self.song_4 = Song("Portishead", "Glory Box")
+        self.song_list = [self.song_1, self.song_2, self.song_3]
 
     def test_room_has_name(self):
-        self.assertEqual("Bowie", self.room_1.name)
+        self.assertEqual("Bowie", self.room.name)
 
 
     def test_song_count(self):
-        self.assertEqual(1, len(self.song_list))
+        self.assertEqual(3, len(self.song_list))
         
     
     def test_add_song_to_list(self):
-        self.room_1.add_song_to_list(self.song_2)
-        self.assertEqual(1, len(self.room_1.song_list))
+        self.room.add_song_to_list(self.song_4)
+        print(self.song_list)
+        self.assertEqual(4, len(self.song_list))
