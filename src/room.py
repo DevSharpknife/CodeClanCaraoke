b0454f904016533e1f2de420e3adc4aa27ea2fea
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-19 14:38:44 +0100

    refactored to create song count test before add song test

--- src/room.py
+++ src/room.py
@@ -1,6 +1,9 @@
 class Room:
 
-    def __init__(self, name):
+    def __init__(self, name, song_list):
         self.name = name
-        self.song_list = []
+        self.song_list = song_list
         self.guest_list = []
+
+    # def add_song_to_song_list(self, song):
+    #     self.song_list.append(song)
--- tests/room_test.py
+++ tests/room_test.py
@@ -6,11 +6,18 @@ from src.song import Song
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        self.room_1 = Room("Bowie")
+        
         self.song_1 = Song("Juice", "Lizzo")
+        self.song_list = [self.song_1]
+
+        self.room_1 = Room("Bowie", self.song_list)
 
     def test_room_has_name(self):
         self.assertEqual("Bowie", self.room_1.name)
 
-    def test_add_song_to_song_list(self):
-        self.assertEqual(1, len(self.room.song_list))
+    def test_song_count(self):
+        self.assertEqual(1, len(self.song_list))
+
+    # def test_add_song_to_song_list(self):
+    #     self.add_song_to_song_list(self.song_1)
+    #     self.assertEqual(1, len(self.song_list))
