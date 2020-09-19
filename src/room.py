2261d19af65787f2d9eebf68d1a72157c4ca5cd6
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-19 15:01:40 +0100

    Readded add song test and function

--- src/room.py
+++ src/room.py
@@ -5,5 +5,6 @@ class Room:
         self.song_list = song_list
         self.guest_list = []
 
-    # def add_song_to_song_list(self, song):
-    #     self.song_list.append(song)
+
+    def add_song_to_song_list(self, song):
+        self.song_list.append(song)
